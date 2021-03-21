0ac5ab92e8ca4b88861262ab51eda153655bcba8
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-03-21 14:43:13 -0300

    Implement EmailJS messaging

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,18 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import emailjs, { init } from 'emailjs-com';
+init("user_0laE1CyljqduXLPSz4BQE");
 
-class Contact extends Component {
-  render() {
+const Contact = () => {
+   const [name, setName] = useState('')
+   const [email, setEmail] = useState('')
+   const [subject, setSubject] = useState('')
+   const [message, setMessage] = useState('')
 
-    if(this.props.data){
-      // var name = this.props.data.name;
-      // var street = this.props.data.address.street;
-      // var city = this.props.data.address.city;
-      // var state = this.props.data.address.state;
-      // var zip = this.props.data.address.zip;
-      // var phone= this.props.data.phone;
-      // var email = this.props.data.email;
-      var message = this.props.data.contactmessage;
-    }
+   const onNameChange = (event) => {
+      setName(event.target.value)
+   }
+
+
+   const onEmailChange = (event) => {
+      setEmail(event.target.value)
+   }
+
+   const onSubjectChange = (event) => {
+      setSubject(event.target.value)
+   }
+
+   const onMsgChange = (event) => {
+      setMessage(event.target.value)
+   }
+
+   function sendEmail(e) {
+      e.preventDefault();
+
+      const messageDetails = {
+      name,
+      email,
+      subject,
+      message
+      }
+
+      console.log("submit button clicked with these message details: ", messageDetails);
+      emailjs.send('service_yltvg0s', 'template_1sxj2o5', messageDetails)
+      .then((result) => {
+            console.log(result);
+            alert("Message Sent."); 
+            resetForm()
+      }, (error) => {
+            console.log(error.text);
+      });
+   }
+
+   const resetForm = () =>{
+      setSubject('')
+      setEmail('')
+      setName('')
+      setMessage('')
+   }
 
     return (
       <section id="contact">
@@ -27,7 +66,7 @@ class Contact extends Component {
 
             <div className="ten columns">
 
-                  <p className="lead">{message}</p>
+                  <p className="lead">Interested to learn more? Get in touch with the form below.</p>
 
             </div>
 
@@ -36,37 +75,38 @@ class Contact extends Component {
          <div className="row">
             <div className="eight columns">
 
-               <form action="" method="post" id="contactForm" name="contactForm">
+            <form action="" method="post" id="contactForm" name="contactForm">
 					<fieldset>
 
                   <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
+						   <input type="text" value={name} size="35" id="contactName" name="contactName" onChange={(event) => onNameChange(event)}/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
+						   <input type="text" value={email} size="35" id="contactEmail" name="contactEmail" onChange={(event) => onEmailChange(event)}/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
+						   <input type="text" size="35" id="contactSubject" name="contactSubject" value={subject} onChange={(event) => onSubjectChange(event)}/>
                   </div>
 
                   <div>
                      <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage" value={message} onChange={(event) => onMsgChange(event)}></textarea>
                   </div>
 
                   <div>
-                     <button className="submit">Submit</button>
+                     <button className="submit" onClick={(e) => sendEmail(e)}>Submit</button>
                      <span id="image-loader">
                         <img alt="" src="images/loader.gif" />
                      </span>
                   </div>
+
 					</fieldset>
-				   </form>
+				</form>
 
            <div id="message-warning"> Error boy</div>
 				   <div id="message-success">
@@ -80,8 +120,8 @@ class Contact extends Component {
             </aside>
       </div>
    </section>
-    );
-  }
+  );
 }
+
 
 export default Contact;
