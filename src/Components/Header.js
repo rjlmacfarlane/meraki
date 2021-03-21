e063a66b6150065dae5bd120b8f73d00f3ad064c
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-03-21 12:31:11 -0300

    Modify hero image and social links

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -34,8 +34,6 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">{name}</h1>
-            <h3>{description}.</h3>
-            <hr />
             <ul className="social">
                {networks}
             </ul>
