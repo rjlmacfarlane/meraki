78801467e8bb4c97736fc1079d14d2c1d3baf980
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-03-24 19:29:14 -0300

    Custom favicon

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,15 +3,17 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      // var occupation= this.props.data.occupation;
-      // var description= this.props.data.description;
-      // var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
-    }
+   //  The following props are passed, but are not used unless needed in the future.
+
+   //  if(this.props.data){
+   //    var name = this.props.data.name;
+   //    var occupation= this.props.data.occupation;
+   //    var description= this.props.data.description;
+   //    var city= this.props.data.address.city;
+   //    var networks= this.props.data.social.map(function(network){
+   //      return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+   //    })
+   //  }
 
     return (
       <header id="home">
