ad4d4f5a0a6eb6b1d4d64c3d722ae3a8d8846cca
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-03-25 12:50:19 -0300

    Update copyright info

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,7 +21,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>Meraki Home Designs and Associated Content &copy; 2021 Iris Chanterelle</li>
+              <li>Meraki Home Design Studio and Associated Content &copy; 2021 Iris Chanterelle</li>
               <li>Developed by <a title="Ryan MacFarlane" href="https://github.com/rjlmacfarlane">Ryan MacFarlane</a></li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
